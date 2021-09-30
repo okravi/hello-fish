03eef31a57f899d00223036d55f6787dad73ab0b
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-09-30 21:29:32 +0300

    update

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // get reference to button
-        val btn_click_me = findViewById(R.id.button) as Button
+        val btnClickMe = findViewById(R.id.button) as Button
         val mytextView = findViewById(R.id.textView) as TextView
         var timesClicked = 0
 // set on-click listener
-        btn_click_me.setOnClickListener {
+        btnClickMe.setOnClickListener {
             timesClicked += 1
             mytextView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
